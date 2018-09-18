521a2d82c34de03978b5c2ef13bbe1b2a216453a
Pontakorn Pakavaleetorn <ppontakorn@gmail.com>
2018-09-18 02:36:11 -0700

    remove comments

--- src/main/java/cs601/project1/AmazonSearch.java
+++ src/main/java/cs601/project1/AmazonSearch.java
@@ -30,22 +30,14 @@ public class AmazonSearch {
 	 * @param args - expects the command -reviews <review_file_name> -qa <qa_file_name>
 	 */
 	public static void main(String[] args) {
-		long start = System.currentTimeMillis();
 		AmazonFileHandling fileHandling = new AmazonFileHandling();
 		if(!fileHandling.isInputValid(args)) {
 			return;
 		}
 		String reviewFileName = args[1];
 		String qaFileName = args[3];
-//		reviewFileName = "testreview.json";
-//		qaFileName = "testqa.json";
-		reviewFileName = "reviews_Cell_Phones_and_Accessories_5.json";
-		qaFileName = "qa_Cell_Phones_and_Accessories.json";
-		
 		InvertedIndex reviewIndex = fileHandling.readFile(reviewFileName, "review");
 		InvertedIndex qaIndex = fileHandling.readFile(qaFileName, "qa");
-		long end = System.currentTimeMillis();
-		System.out.println("Time: " + Math.round((end-start) / 1000) + " seconds" );
 		try(
 			Scanner scanner = new Scanner(System.in);
 		){
